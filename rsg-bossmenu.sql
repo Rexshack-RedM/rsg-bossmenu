6a7620100493cd5e8182a127cf3864d1ac5cb026
RexshackGaming <rexshackuk@gmail.com>
2023-12-22 07:29:36 +0000

    Update rsg-bossmenu.sql

--- rsg-bossmenu.sql
+++ rsg-bossmenu.sql
@@ -9,7 +9,6 @@ KEY `type` (`type`)
 );
 
 INSERT INTO `management_funds` (`job_name`, `amount`, `type`) VALUES
-('police', 0, 'boss'),
 ('vallaw', 0, 'boss'),
 ('rholaw', 0, 'boss'),
 ('blklaw', 0, 'boss'),
